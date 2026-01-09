869cb952ad3a46738dc5dd979ded38af20e96ef2
sagarboyal <sboyal12@gmail.com>
2026-01-09 12:00:18 +0530

    project created via vite

---
+++ src/main.tsx
@@ -0,0 +1,8 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+     <text>Hello world</text>
+  </StrictMode>,
+)
