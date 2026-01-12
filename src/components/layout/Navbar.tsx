fe90948d0aaf2cde99ca6c3f4b900cd6a6f03d23
Koushik Guria <koushikguria998@gmail.com>
2026-01-12 19:19:58 +0530

    Add deps (tailwind css, react icon) and initial app setup

---
+++ src/components/layout/Navbar.tsx
@@ -0,0 +1,12 @@
+import { FiSearch } from "react-icons/fi";
+
+
+function Navbar() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      Koushik
+    </div>
+  );
+}
+
+export default Navbar;
