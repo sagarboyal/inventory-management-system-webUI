189db4009f6092c1c5af2919efa973df00691267
Koushik Guria <koushikguria998@gmail.com>
2026-01-13 19:24:11 +0530

    Initial Navbar setup

---
+++ src/constants/images.ts
@@ -0,0 +1,11 @@
+import logo from "../assets/images/logo.png";
+import food_img from "../assets/images/food_img.png";
+import flag_img from "../assets/images/flag_img.png";
+import avatar_img from "../assets/images/avatar_img.png";
+
+export const images = {
+    logo,
+    food_img,
+    flag_img,
+    avatar_img,
+};
